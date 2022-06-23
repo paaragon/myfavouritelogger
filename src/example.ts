dcf27cf9bef45b584b9155872d24487a8e265c28
Pablo Aragón <pablo.aragon22@gmail.com>
2022-06-23 11:22:59 +0200

    chore: update dependencies

---
+++ src/example.ts
@@ -0,0 +1,8 @@
+import myfavouritelogger from './myfavouritelogger';
+
+const logger = myfavouritelogger();
+
+logger.info('Hola');
+logger.error('Hola');
+logger.warn('Hola');
+logger.debug('Hola');
